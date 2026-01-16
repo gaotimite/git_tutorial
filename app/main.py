5807136677c4b262a5dfde090aebdf75e89fa4b4
gaotimite <gaotimite@gmail.com>
2026-01-16 14:01:26 +0000

    add a add_number method

--- app/main.py
+++ app/main.py
@@ -10,3 +10,7 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
+
+@app.get("/add_number/{number1}/{number2}")
+def add_number(number1: int, number2: int):
+    return number1 + number2
